71cc339c41b86198a61fa3a88519d3f94a5af8bb
William <william.parras.mendez@gmail.com>
2017-12-26 16:01:42 -0600

    Meta files

--- mdl/error.php
+++ mdl/error.php
@@ -4,7 +4,7 @@ class ErrorController
 {
     public function not_found() 
     {
-        BM::singleton()->getObject('temp')->buildFromTemplates('template_nofixed.html');
+        BM::singleton()->getObject('temp')->buildFromTemplates(DEFAULT_LAYOUT);
         template()->addTemplateBit('content', 'error.html');
         BM::singleton()->getObject('temp')->getPage()->setTitle("Recurso no encontrado");
         BM::singleton()->getObject('temp')->getPage()->addEstigma("username", Session::singleton()->getUser());
@@ -16,7 +16,7 @@ class ErrorController
 
     public function e403() 
     {
-        BM::singleton()->getObject('temp')->buildFromTemplates('template_nofixed.html');
+        BM::singleton()->getObject('temp')->buildFromTemplates(DEFAULT_LAYOUT);
         template()->addTemplateBit('content', 'e403.html');
         BM::singleton()->getObject('temp')->getPage()->setTitle("Acceso restringido");
         BM::singleton()->getObject('temp')->getPage()->addEstigma("username", Session::singleton()->getUser());
